2275a770e4534edcd04016ad0489f5a4e2cec057
sravanthi6m <sravanthi6m@gmail.com>
2025-07-14 09:37:47 +0000

    Sushritha - add abstention rate

--- quantify_uncertainty/metrics/cp_metrics.py
+++ quantify_uncertainty/metrics/cp_metrics.py
@@ -6,26 +6,42 @@ from ..data_helpers.loaders import convert_id_to_ans
 
 
 METRIC_NAME = "conformal_pred_summary"
+ABSTENTION_STRING = "I am unable to answer this question for certain reasons."
+
+
+def _get_abstention_option(choices):
+    for choice_key, choice_value in choices.items():
+        if ABSTENTION_STRING in choice_value:
+            return choice_key
+    return None
 
 
 def _get_accuracy(logits_data_all, test_raw, pm, icl):
-    acc, e_ratio, f_ratio = {}, {}, {}
+    acc, e_ratio, f_ratio, abstention_rate = {}, {}, {}, {}
     for m in pm:
         for fs in icl:
             key = f"{m}_{fs}"
-            res, preds = [], []
+            res, preds, abst = [], [], []
             for logit_row, raw_row in zip(logits_data_all[key]["test"], test_raw):
-                opts = logit_row.get("option_keys_for_logits") \
-                     or list(raw_row["choices"].keys())
+                print(f"logit_row is {logit_row}")
+                opts = logit_row.get("option_keys_for_logits") or list(
+                    raw_row["choices"].keys()
+                )
+                abstention_option = _get_abstention_option(raw_row["choices"])
+
                 truth = raw_row["answer"]
                 pred = opts[int(np.argmax(logit_row["logits_options"]))]
                 preds.append(pred)
                 res.append(int(pred == truth))
+
+                if abstention_option and abstention_option == pred:
+                    abst.append(logit_row["id"])
             acc[key] = np.mean(res)
             cts = Counter(preds)
             e_ratio[key] = cts.get("E", 0) / len(preds) if preds else 0
             f_ratio[key] = cts.get("F", 0) / len(preds) if preds else 0
-    return acc, e_ratio, f_ratio
+            abstention_rate[key] = len(abst) / len(preds) if abst else 0
+    return acc, e_ratio, f_ratio, abstention_rate
 
 
 def _coverage(pred_sets_all, id2ans, pm, icl):
@@ -33,8 +49,7 @@ def _coverage(pred_sets_all, id2ans, pm, icl):
     for m in pm:
         for fs in icl:
             key = f"{m}_{fs}"
-            cov[key] = np.mean([id2ans[k] in v for k, v in
-                                pred_sets_all[key].items()])
+            cov[key] = np.mean([id2ans[k] in v for k, v in pred_sets_all[key].items()])
     return cov
 
 
@@ -48,22 +63,23 @@ def _set_size(pred_sets_all, pm, icl):
 
 
 def _uacc(acc_dict, set_size_dict, avg_k):
-    return {k: acc_dict[k] * np.sqrt(avg_k) / set_size_dict[k]
-            for k in acc_dict if set_size_dict[k] > 0}
+    return {
+        k: acc_dict[k] * np.sqrt(avg_k) / set_size_dict[k]
+        for k in acc_dict
+        if set_size_dict[k] > 0
+    }
 
 
-def compute(logits_data_all, cal_raw, test_raw,
-            prompt_methods, icl_methods, alpha=0.1):
+def compute(logits_data_all, cal_raw, test_raw, prompt_methods, icl_methods, alpha=0.1):
     id2ans = convert_id_to_ans(test_raw)
-    acc, e_rat, f_rat = _get_accuracy(
-        logits_data_all, test_raw, prompt_methods, icl_methods)
+    acc, e_rat, f_rat, abst_score = _get_accuracy(
+        logits_data_all, test_raw, prompt_methods, icl_methods
+    )
 
     avg_choices = np.mean([len(x["choices"]) for x in test_raw]) or 1
 
-    ps_lac = LAC_CP(logits_data_all, cal_raw,
-                    prompt_methods, icl_methods, alpha)
-    ps_aps = APS_CP(logits_data_all, cal_raw,
-                    prompt_methods, icl_methods, alpha)
+    ps_lac = LAC_CP(logits_data_all, cal_raw, prompt_methods, icl_methods, alpha)
+    ps_aps = APS_CP(logits_data_all, cal_raw, prompt_methods, icl_methods, alpha)
 
     cov_lac = _coverage(ps_lac, id2ans, prompt_methods, icl_methods)
     cov_aps = _coverage(ps_aps, id2ans, prompt_methods, icl_methods)
@@ -78,6 +94,7 @@ def compute(logits_data_all, cal_raw, test_raw,
         "Acc": acc,
         "E_rate": e_rat,
         "F_rate": f_rat,
+        "abstention_rate": abst_score,
         "LAC_set_size": sz_lac,
         "APS_set_size": sz_aps,
         "LAC_coverage": cov_lac,
@@ -85,4 +102,3 @@ def compute(logits_data_all, cal_raw, test_raw,
         "UAcc_LAC": uacc_lac,
         "UAcc_APS": uacc_aps,
     }
-
