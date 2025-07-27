b4164071c0b91d86a18723280b4dce66115e6daa
sravanthi6m <sravanthi6m@gmail.com>
2025-07-27 19:11:02 +0000

    Refactor for removing forced internal filename format

--- quantify_uncertainty/eval.py
+++ quantify_uncertainty/eval.py
@@ -12,32 +12,24 @@ def _discover_metric_modules():
 
 
 def evaluate(cfg):
-    results = {}
     metric_mods = list(_discover_metric_modules())
 
-    for dsets in cfg.data_names:
-        cal_raw, test_raw = get_raw_data(cfg.raw_data_dir, dsets, cfg.cal_ratio)
-        
-        logits_all = get_logits_data(
-            model_name=cfg.model, 
-            data_name=dsets, 
-            cal_raw_data=cal_raw, 
-            test_raw_data=test_raw,
-            logits_data_dir=cfg.logits_data_dir, 
-            cal_ratio=cfg.cal_ratio,
-            prompt_methods=cfg.prompt_methods, 
-            icl_methods=cfg.icl_methods,
-            k_few_shot=cfg.k_few_shot,
-            cot=cfg.cot
-        )
+    cal_raw, test_raw = get_raw_data(cfg.raw_data_file, cfg.cal_ratio)
+    
+    logits_all = get_logits_data(
+        logits_pkl_path=cfg.logits_pkl_path,
+        cal_ratio=cfg.cal_ratio,
+        prompt_methods=cfg.prompt_methods, 
+        icl_methods=cfg.icl_methods
+    )
 
-        per_dataset = {}
-        for mod in metric_mods:
-            out = mod.compute(logits_all, cal_raw, test_raw,
-                              cfg.prompt_methods, cfg.icl_methods,
-                              alpha=cfg.alpha)
-            per_dataset.update({f"{mod.METRIC_NAME}.{k}": v
-                                for k, v in out.items()})
-        results[dsets] = per_dataset
-    return results
+    per_dataset_results = {}
+    for mod in metric_mods:
+        out = mod.compute(logits_all, cal_raw, test_raw,
+                            cfg.prompt_methods, cfg.icl_methods,
+                            alpha=cfg.alpha)
+        per_dataset_results.update({f"{mod.METRIC_NAME}.{k}": v
+                            for k, v in out.items()})
+    
+    return per_dataset_results
 
